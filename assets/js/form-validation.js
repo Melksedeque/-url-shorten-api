36e13984ffe78d1151e4272a834af5903bb409ae
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-04-15 22:59:32 -0300

    input.classList.add function added to add class error in validation input

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,15 +1,15 @@
 (() => {
     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
+    const input = document.querySelector('input[name="link"]')
   
-    // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
         form.addEventListener('submit', event => {
             if (!form.checkValidity()) {
                 event.preventDefault()
                 event.stopPropagation()
+                input.classList.add('error')
             }
 
             form.classList.add('was-validated')
