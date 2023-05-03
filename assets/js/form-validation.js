358f9412bcc4281702d6ad07f0907038f8ef0119
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-05-03 12:46:50 -0300

    Validation created in the main.js file.

--- assets/js/form-validation.js
+++
@@ -1,9 +0,0 @@
-form.addEventListener('submit', (e) => {
-    if (!form.checkValidity()) {
-        e.preventDefault()
-        e.stopPropagation()
-        input.classList.add('error')
-    }
-
-    form.classList.add('was-validated')
-}, false);
