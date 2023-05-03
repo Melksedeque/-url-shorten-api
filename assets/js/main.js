5e4ed3581ccc8f2dd935322eefca00e45ffee93a
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-05-03 12:47:38 -0300

    Form validation and ClipboardJS object created on main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,25 @@
-function copyText() {
-    const item = document.querySelector('ul.links-list li span');
-  
-    item.select();
-    item.setSelectionRange(0, 99999);
-  
-    navigator.clipboard.writeText(item.innerText);
-  
-    console.log("Texto copiado: " + item.innerText);
-} 
+const form = document.querySelector('form.needs-validation');
+const input = document.querySelector('input[name="link"]');
+const buttonsCopy = document.querySelectorAll('button.btn-copy');
+const btnSubmit = document.querySelector('button[type="submit"]');
 
+let linksList = document.querySelector('ul.links-list');
+
+if (localStorage.getItem('linksList')) {
+    linksList.innerHTML = JSON.parse(localStorage.getItem('linksList'));
+}
+
+// I used the Clipboard.JS library because my browser does not support the object navigator.clipboard
+new ClipboardJS('button.btn-copy');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!form.checkValidity()) {
+        e.stopPropagation();
+        input.classList.add('error');
+    }
+
+    form.classList.add('was-validated');
+
+    shortenUrl();
+}, false);
